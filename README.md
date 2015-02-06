b0c984f4d97f97952f6677aabe4767b0a68618db
pjv <ezelspinguin@gmail.com>
2015-02-06 01:22:01 +0100

    First basic working version.

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-Review Breakdown
-================
+App Router Test
+===============
 
-A component for review breakdowns like at The Verge.
+Tests, demo and documentation for app-router. 
+Mostly auto-generated Dart translation of tests of app-router-dart that are compatible with Polymer.dart.
 
-This Polymer custom element uses Dart.
+This is still EXPERIMENTAL and untested.
+
+See a demo and documentation at https://pjjjv.github.io/app-router-test/demo.html.
+
+By pjv <ezelspinguin@gmail.com>. Original author is Erik Ringsmuth <erik.ringsmuth@gmail.com>.
--- web/demo.dart
+++ web/demo.dart
@@ -1,7 +1,25 @@
+/*
+ *  App Router Test - dart
+ *  Copyright (c) 2015 pjv
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import 'dart:html';
 import 'package:polymer/polymer.dart';
 import 'package:template_binding/template_binding.dart';
-import 'package:pushstate_anchor_dart/pushstate_anchor.dart';
+import 'package:pushstate_anchor/pushstate_anchor.dart';
 
 void main() {
   initPolymer().run(() {
